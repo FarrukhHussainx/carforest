f1e9b01dcd55d21d60b1ed2a66bcd4676e9b8c2c
FarrukhHussainx <fa4ukhhuxxain101@gmail.com>
2023-08-11 15:02:13 +0500

    loginfix

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -28,132 +28,132 @@ export default function Component() {
     router.push("/placeadd");
   }
   if (session.status === "unauthenticated") {
-  return (
-    <>
-      <div className="hidden h-screen w-[100%] md:flex justify-around items-center ">
-        <div className="flex flex-col">
-          <div className="flex z-10 gap-2">
-            <Image src={main} width={50} height={50} alt="car-forest" />
-            <h1 className="font-bold font-mono text-3xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-800">
-              Car-Forest
-            </h1>
-          </div>
-          <p className="mt-4 text-gray-500 xl:text-2xl 2xl:text-3xl">
-            Explore our wide range of high-quality cars
-          </p>
-        </div>
-        <div className="w-[35%] border bg-white rounded-md shadow-lg">
-          <div className="mx-auto w-10/12 mt-5 mb-5">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="email"
-                name="email"
-                //   value={carData.price}
-                //   onChange={handleChange}
-                placeholder="Email"
-                className="w-full px-4 py-2 mb-3 border rounded "
-              />
-              <input
-                type="password"
-                name="password"
-                //   value={carData.price}
-                //   onChange={handleChange}
-                placeholder="Password"
-                className="w-full px-4 py-2 mb-3 border rounded"
-              />
-              <button
-                className="w-full bg-blue-600 text-white rounded-md p-3"
-                type="submit"
-              >
-                Login
-              </button>
-            </form>
-            <p className="text-gray-500 flex justify-end items-center mt-1">
-              Don't have account,
-              <Link href="/register" className="text-blue-600">
-                Rgister
-              </Link>
+    return (
+      <>
+        <div className="hidden h-screen w-[100%] md:flex justify-around items-center ">
+          <div className="flex flex-col">
+            <div className="flex z-10 gap-2">
+              <Image src={main} width={50} height={50} alt="car-forest" />
+              <h1 className="font-bold font-mono text-3xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-800">
+                Car-Forest
+              </h1>
+            </div>
+            <p className="mt-4 text-gray-500 xl:text-2xl 2xl:text-3xl">
+              Explore our wide range of high-quality cars
             </p>
-            <hr className="w-full mt-6 mb-6" />
-            <button
-              className="w-6/12 bg-blue-600 text-white rounded-md  mx-auto flex items-center p-1 "
-              onClick={() => signIn("google")}
-            >
-              <div className="flex items-center justify-center">
-                <div className="bg-white p-1 border  rounded-md">
-                  <Image
-                    src="https://img.freepik.com/free-icon/google_318-278809.jpg?w=360"
-                    width={25}
-                    height={25}
-                    alt="freepik"
-                  />
-                </div>
-                <p className="ml-1"> Sign in with Google</p>
-              </div>
-            </button>
           </div>
-        </div>
-      </div>
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      <div className="h-screen w-[100%] flex justify-around items-center md:hidden">
-        <div className="w-[80%] border bg-white rounded-md shadow-lg">
-          <div className="mx-auto w-10/12 mt-5 mb-5">
-            <form onSubmit={handleSubmit}>
-              <input
-                type="email"
-                name="email"
-                //   value={carData.price}
-                //   onChange={handleChange}
-                placeholder="Email"
-                className="w-full px-4 py-2 mb-3 border rounded "
-              />
-              <input
-                type="password"
-                name="password"
-                //   value={carData.price}
-                //   onChange={handleChange}
-                placeholder="Password"
-                className="w-full px-4 py-2 mb-3 border rounded"
-              />
+          <div className="w-[35%] border bg-white rounded-md shadow-lg">
+            <div className="mx-auto w-10/12 mt-5 mb-5">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="email"
+                  name="email"
+                  //   value={carData.price}
+                  //   onChange={handleChange}
+                  placeholder="Email"
+                  className="w-full px-4 py-2 mb-3 border rounded "
+                />
+                <input
+                  type="password"
+                  name="password"
+                  //   value={carData.price}
+                  //   onChange={handleChange}
+                  placeholder="Password"
+                  className="w-full px-4 py-2 mb-3 border rounded"
+                />
+                <button
+                  className="w-full bg-blue-600 text-white rounded-md p-3"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </form>
+              <p className="text-gray-500 flex justify-end items-center mt-1">
+                Don't have account,
+                <Link href="/register" className="text-blue-600">
+                  Rgister
+                </Link>
+              </p>
+              <hr className="w-full mt-6 mb-6" />
               <button
-                className="w-full bg-blue-600 text-white rounded-md p-3"
-                type="submit"
+                className="w-6/12 bg-blue-600 text-white rounded-md  mx-auto flex items-center p-1 "
+                onClick={() => signIn("google")}
               >
-                Login
-              </button>
-            </form>
-            <p className="text-gray-500 flex justify-end items-center mt-1">
-              Don't have account,
-              <Link href="/register" className="text-blue-600">
-                Rgister
-              </Link>
-            </p>
-            <hr className="w-full mt-6 mb-6" />
-            <button
-              className="w-7/12 bg-blue-600 text-white rounded-md  mx-auto flex items-center p-1 "
-              onClick={() => signIn("google")}
-            >
-              <div className="flex items-center justify-center">
-                <div className="bg-white p-1 border  rounded-md">
-                  <Image
-                    src="https://img.freepik.com/free-icon/google_318-278809.jpg?w=360"
-                    width={25}
-                    height={25}
-                    alt="freepik"
-                  />
+                <div className="flex items-center justify-center">
+                  <div className="bg-white p-1 border  rounded-md">
+                    <Image
+                      src="https://img.freepik.com/free-icon/google_318-278809.jpg?w=360"
+                      width={25}
+                      height={25}
+                      alt="freepik"
+                    />
+                  </div>
+                  <p className="ml-1"> Sign in with Google</p>
                 </div>
-                <p className="ml-1">Google SignIn</p>
-              </div>
-            </button>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+        {/*  */}
+        {/*  */}
+        {/*  */}
+        <div className="h-screen w-[100%] flex justify-around items-center md:hidden">
+          <div className="w-[80%] border bg-white rounded-md shadow-lg">
+            <div className="mx-auto w-10/12 mt-5 mb-5">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="email"
+                  name="email"
+                  //   value={carData.price}
+                  //   onChange={handleChange}
+                  placeholder="Email"
+                  className="w-full px-4 py-2 mb-3 border rounded "
+                />
+                <input
+                  type="password"
+                  name="password"
+                  //   value={carData.price}
+                  //   onChange={handleChange}
+                  placeholder="Password"
+                  className="w-full px-4 py-2 mb-3 border rounded"
+                />
+                <button
+                  className="w-full bg-blue-600 text-white rounded-md p-3"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </form>
+              <p className="text-gray-500 flex justify-end items-center mt-1">
+                Don't have account,
+                <Link href="/register" className="text-blue-600">
+                  Rgister
+                </Link>
+              </p>
+              <hr className="w-full mt-6 mb-6" />
+              <button
+                className="w-7/12 bg-blue-600 text-white rounded-md  mx-auto flex items-center p-1 "
+                onClick={() => signIn("google")}
+              >
+                <div className="flex items-center justify-center">
+                  <div className="bg-white p-1 border  rounded-md">
+                    <Image
+                      src="https://img.freepik.com/free-icon/google_318-278809.jpg?w=360"
+                      width={25}
+                      height={25}
+                      alt="freepik"
+                    />
+                  </div>
+                  <p className="ml-1">Google SignIn</p>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-// }
 
 // const { data: session } = useSession();
 // console.log(session);
